f76ec800d414250328742ad45147a976725d51c3
Jaeho Jeon <88sprout@gmail.com>
2024-06-02 00:18:44 +0900

    segment_place -> place

--- 프로젝트_3번/main.c
+++ 프로젝트_3번/main.c
@@ -3,13 +3,14 @@
 int encoder_cnt = 0;
 int password = 0;
 int key = 0; // keypad 에서 입력한 값을 저장하는 임시변수.
-int segment_place = 0; // 4자리 segment 의 각 자리의 현재 선택을 의미, hardware: |0 1 2 3|
+int place = 0; // 4자리 segment 의 각 자리의 현재 선택을 의미, hardware: |0 1 2 3|
 int data[4] = {0,}; // data: |0,0,0,0|, data[십진자릿수]
 //pwm_data = data[3] * 1000 + data[2] * 100 + data[1] * 10 + data[0];
 unsigned int i = 0;
 
 void init(void);
 void keypad_controller(void);
+void show_screen(void);
 
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
@@ -90,9 +91,9 @@ void keypad_controller(void){
     }
     else if ((P6IN & BIT4) == 0) // Button *
     {
-        while ((P6IN & BIT4) == 0) // * 을 누르면, 다음 segment_place 로 이동
-            key = 0; // segment_place 이동 전 key 초기화
-        segment_place++; // segment_place 이동
+        while ((P6IN & BIT4) == 0) // * 을 누르면, 다음 place 로 이동
+            key = 0; // place 이동 전 key 초기화
+        place++; // place 이동
     }
 
     // columns 2
@@ -134,7 +135,7 @@ void keypad_controller(void){
     }
     else if ((P6IN & BIT4) == 0) // Button # 을 누르면 전체 password data 초기화
     {
-        segment_place = 0; // segment_place 초기화
+        place = 0; // place 초기화
 
         for (i = 0; i < 4; i++) // data 초기화
         {
@@ -142,24 +143,24 @@ void keypad_controller(void){
         }
     }
 
-    // data[segment_place] 에 key 를 저장.
-    if (segment_place == 0)
+    // data[place] 에 key 를 저장.
+    if (place == 0)
     {
         data[3] = key;
     }
-    else if (segment_place == 1)
+    else if (place == 1)
     {
         data[2] = key;
     }
-    else if (segment_place == 2)
+    else if (place == 2)
     {
         data[1] = key;
     }
-    else if (segment_place == 3)
+    else if (place == 3)
     {
         data[0] = key;
     }
-    else if (segment_place == 4) // password 입력 완료
+    else if (place == 4) // password 입력 완료
     {
         
     }
