bc8c14db7d4f5f817120abcd7863eb968959a87b
Jaeho Jeon <88sprout@gmail.com>
2024-06-01 21:58:51 +0900

    keypad controller

--- 프로젝트_3번/main.c
+++ 프로젝트_3번/main.c
@@ -29,6 +29,123 @@ void main(void) {
     P1IFG &= ~BIT3; // Interrupt flag
 
     __bis_SR_register(GIE); // Interrupt enable
+
+    while (1)
+    {
+
+        /*Keypad Controller*/
+
+        // columns 1
+        P2OUT &= ~BIT2;
+        P2OUT |= (BIT0 | BIT1 | BIT3);
+
+        if ((P6IN & BIT3) == 0) // Button 1
+        {
+            key = 1;
+        }
+        else if ((P6IN & BIT6) == 0) // Button 4
+        {
+            key = 4;
+        }
+        else if ((P6IN & BIT5) == 0) // Button 7
+        {
+            key = 7;
+        }
+        else if ((P6IN & BIT4) == 0) // Button *
+        {
+            while ((P6IN & BIT4) == 0)
+                key = 0; // Assuming key = 10 for '*'
+            menu++;
+        }
+
+        // columns 2
+        P2OUT &= ~BIT0;
+        P2OUT |= (BIT2 | BIT3);
+
+        if ((P6IN & BIT3) == 0) // Button 2
+        {
+            key = 2;
+        }
+        else if ((P6IN & BIT6) == 0) // Button 5
+        {
+            key = 5;
+        }
+        else if ((P6IN & BIT5) == 0) // Button 8
+        {
+            key = 8;
+        }
+        else if ((P6IN & BIT4) == 0) // Button 0
+        {
+            key = 0;
+        }
+
+        // columns 3
+        P2OUT &= ~BIT1;
+        P2OUT |= (BIT0 | BIT2);
+
+        if ((P6IN & BIT3) == 0) // Button 3
+        {
+            key = 3;
+        }
+        else if ((P6IN & BIT6) == 0) // Button 6
+        {
+            key = 6;
+        }
+        else if ((P6IN & BIT5) == 0) // Button 9
+        {
+            key = 9;
+        }
+        else if ((P6IN & BIT4) == 0) // Button #
+        {
+            menu = 0;
+            TA2CCR2 = 0;
+            TA2CCR1 = 0;
+
+            for (i = 0; i < 4; i++)
+            {
+                data[i] = 0;
+            }
+        }
+
+        // save key_number
+        if (menu == 0)
+        {
+            data[3] = key;
+        }
+        else if (menu == 1)
+        {
+            data[2] = key;
+        }
+        else if (menu == 2)
+        {
+            data[1] = key;
+        }
+        else if (menu == 3)
+        {
+            data[0] = key;
+        }
+        else if (menu == 4)
+        {
+            pwm_data = data[3] * 1000 + data[2] * 100 + data[1] * 10 + data[0];
+
+            if (pwm_data < 1000)
+            {
+                TA2CCR2 = pwm_data;
+                TA2CCR1 = 0;
+            }
+            else
+            {
+                TA2CCR2 = 0;
+                TA2CCR1 = 0;
+                for (i = 0; i < 4; i++)
+                {
+                    data[i] = 0;
+                }
+            }
+        }
+    }
+    /* END Keypad Controller*/
+
 }
 
 #pragma vector=PORT1_VECTOR
