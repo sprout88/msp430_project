198da84e66bd52147f758e7163d42b738d3ba90c
Jaeho Jeon <88sprout@gmail.com>
2024-06-01 22:10:38 +0900

    Pasword System Refectr

--- 프로젝트_3번/main.c
+++ 프로젝트_3번/main.c
@@ -2,6 +2,12 @@
 
 int encoder_cnt = 0;
 int password = 0;
+int key = 0; // keypad 에서 입력한 값을 저장하는 임시변수.
+int segment_place = 0; // 4자리 segment 의 각 자리의 현재 선택을 의미, hardware: |0 1 2 3|
+int data[4] = {0,}; // data: |0,0,0,0|, data[십진자릿수]
+//pwm_data = data[3] * 1000 + data[2] * 100 + data[1] * 10 + data[0];
+
+unsigned int i = 0;
 
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
@@ -53,9 +59,9 @@ void main(void) {
         }
         else if ((P6IN & BIT4) == 0) // Button *
         {
-            while ((P6IN & BIT4) == 0)
-                key = 0; // Assuming key = 10 for '*'
-            menu++;
+            while ((P6IN & BIT4) == 0) // * 을 누르면, 다음 segment_place 로 이동
+                key = 0; // segment_place 이동 전 key 초기화
+            segment_place++; // segment_place 이동
         }
 
         // columns 2
@@ -95,36 +101,34 @@ void main(void) {
         {
             key = 9;
         }
-        else if ((P6IN & BIT4) == 0) // Button #
+        else if ((P6IN & BIT4) == 0) // Button # 을 누르면 전체 password data 초기화
         {
-            menu = 0;
-            TA2CCR2 = 0;
-            TA2CCR1 = 0;
+            segment_place = 0; // segment_place 초기화
 
-            for (i = 0; i < 4; i++)
+            for (i = 0; i < 4; i++) // data 초기화
             {
                 data[i] = 0;
             }
         }
 
-        // save key_number
-        if (menu == 0)
+        // data[segment_place] 에 key 를 저장.
+        if (segment_place == 0)
         {
             data[3] = key;
         }
-        else if (menu == 1)
+        else if (segment_place == 1)
         {
             data[2] = key;
         }
-        else if (menu == 2)
+        else if (segment_place == 2)
         {
             data[1] = key;
         }
-        else if (menu == 3)
+        else if (segment_place == 3)
         {
             data[0] = key;
         }
-        else if (menu == 4)
+        else if (segment_place == 4)
         {
             pwm_data = data[3] * 1000 + data[2] * 100 + data[1] * 10 + data[0];
 
