ac5c18a61dfcfe8619abe96f038d0634ce72fb6e
Jaeho Jeon <88sprout@gmail.com>
2024-06-03 23:02:04 +0900

    refctr : extract LED functions

--- _ref/9-1_adc_single.c
+++ _ref/9-1_adc_single.c
@@ -8,12 +8,28 @@ void init_led1(void);
 void init_led2(void);
 void init_ADC(void);
 
+void turn_on_led(int led_num);
+void turn_off_led(int led_num);
+
 void main(void)
 {
-    
+    init_led1();
+    init_led2();
+
+    init_ADC();
+
+    while(1){
+        ADC12CTL0 |= ADC12SC;
+        while(!(ADC12IFG & BIT0));
+        data = ADC12MEM0;
+
+        if(data > 3000)
+        {
+        }
+    }
 }
 
-void init_led1(void){
+void init_led(){
     // LED1 DIR : P1.1
     P1DIR |= BIT0;   
     // LED1 OFF
@@ -25,6 +41,23 @@ void init_led2(void){
     P4DIR |= BIT7;
     // LED2 OFF
     P4OUT &= ~BIT7; // LED2 OFF
+}
+
+void turn_on_led(int led_num){
+    switch led_num{
+        case(1):
+            P1OUT |= BIT0; // LED1 ON
+        case(2):
+            P4OUT |= BIT7; // LED2 ON
+    }
+}
+void turn_off_led(int led_num){
+    switch led_num{
+        case(1):
+            P1OUT &= ~BIT0; // LED1 OFF
+        case(2):
+            P4OUT &= ~BIT7; // LED2 OFF
+    }
 }
 
 void init_ADC(void){
