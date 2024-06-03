4767d24c41e6703e690c890d477580dd36de3287
Jaeho Jeon <88sprout@gmail.com>
2024-06-03 23:11:48 +0900

    TESTED : adc 에 따라 led 순차 점등

--- _ref/9-1_adc_single.c
+++ _ref/9-1_adc_single.c
@@ -1,11 +1,14 @@
+/*
+ADC 를 돌리면, LED 가 순차적으로 점등한다.
+*/
+
 #include <msp430.h>
 
 unsigned int data = 0;
 
 void stop_watchdog_timer(void);
 
-void init_led1(void);
-void init_led2(void);
+void init_led(int led_num);
 void init_ADC(void);
 
 void turn_on_led(int led_num);
@@ -13,8 +16,8 @@ void turn_off_led(int led_num);
 
 void main(void)
 {
-    init_led1();
-    init_led2();
+    init_led(1);
+    init_led(2);
 
     init_ADC();
 
@@ -25,38 +28,56 @@ void main(void)
 
         if(data > 3000)
         {
+            turn_on_led(1);
+            turn_on_led(2);
+        }
+        else if(data > 2000)
+        {
+            turn_off_led(1);
+            turn_on_led(2);
+        }
+        else // data < 2000
+        {
+            turn_off_led(1);
+            turn_off_led(2);
         }
     }
 }
 
-void init_led(){
-    // LED1 DIR : P1.1
-    P1DIR |= BIT0;   
-    // LED1 OFF
-    P1OUT &= ~BIT0;
-}
-
-void init_led2(void){
-    // LED2 DIR : P4.7
-    P4DIR |= BIT7;
-    // LED2 OFF
-    P4OUT &= ~BIT7; // LED2 OFF
+void init_led(int led_num){
+    switch(led_num){
+        case 1:
+            // LED1 DIR : P1.1
+            P1DIR |= BIT0;
+            // LED1 OFF
+            P1OUT &= ~BIT0;
+            break;
+        case 2:
+            // LED2 DIR : P4.7
+            P4DIR |= BIT7;
+            // LED2 OFF
+            P4OUT &= ~BIT7; // LED2 OFF
+    }
 }
 
 void turn_on_led(int led_num){
-    switch led_num{
-        case(1):
+    switch(led_num){
+        case 1:
             P1OUT |= BIT0; // LED1 ON
-        case(2):
+            break;
+        case 2:
             P4OUT |= BIT7; // LED2 ON
+            break;
     }
 }
 void turn_off_led(int led_num){
-    switch led_num{
-        case(1):
+    switch(led_num){
+        case 1:
             P1OUT &= ~BIT0; // LED1 OFF
-        case(2):
+            break;
+        case 2:
             P4OUT &= ~BIT7; // LED2 OFF
+            break;
     }
 }
 
@@ -64,7 +85,7 @@ void init_ADC(void){
     P6SEL |= BIT0; // ADC DIR
 
     /* ADC12 control register set */
-    /* sample hold time : 16 adc clock cycles, ADC12 ON */ 
+    /* sample hold time : 16 adc clock cycles, ADC12 ON */
     ADC12CTL0 = ADC12SHT02 + ADC12ON;
 
     ADC12CTL1 = ADC12SHP; // sample hold : pulse mode
