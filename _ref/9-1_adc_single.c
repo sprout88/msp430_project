d516e546349e9d6f18727afe1b7eee4aa6e3f04f
Jaeho Jeon <88sprout@gmail.com>
2024-06-03 22:54:44 +0900

    init_ADC

--- _ref/9-1_adc_single.c
+++ _ref/9-1_adc_single.c
@@ -6,14 +6,10 @@ void stop_watchdog_timer(void);
 
 void init_led1(void);
 void init_led2(void);
+void init_ADC(void);
 
 void main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;
-
-    P1DIR |= BIT0;
-    P4DIR |= BIT7;
-
     
 }
 
@@ -29,4 +25,16 @@ void init_led2(void){
     P4DIR |= BIT7;
     // LED2 OFF
     P4OUT &= ~BIT7; // LED2 OFF
+}
+
+void init_ADC(void){
+    P6SEL |= BIT0; // ADC DIR
+
+    /* ADC12 control register set */
+    /* sample hold time : 16 adc clock cycles, ADC12 ON */ 
+    ADC12CTL0 = ADC12SHT02 + ADC12ON;
+
+    ADC12CTL1 = ADC12SHP; // sample hold : pulse mode
+    ADC12MCTL0 = ADC12INCH_0; // input channel=A0
+    ADC12CTL0 |= ADC12ENC; // ADC12 encoding=enable
 }
