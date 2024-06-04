6c7fdc9384c9843d2a66e17fcf99c8357abbc635
Jaeho Jeon <88sprout@gmail.com>
2024-06-04 15:45:55 +0900

    원인 규명 : watch_dog timer 에 의해 전역변수가 초기값으로 reset 되는 문제임

---
+++ _test/2-1_global_var_error-change_temp_with_interrupt_handler.c
@@ -0,0 +1,83 @@
+/* 오류 발생 코드 */
+
+/* */
+
+#include <msp430.h>
+#define ADC_DELTA_TEN_TIME 1507
+#define ADC_MIN 1081
+#define ADC_MAX 4095
+
+unsigned int temp = 4;
+
+/* watchdog timer functions */
+void stop_watchdog_timer(void);
+
+/* switch functions */
+void init_right_switch(void);
+void right_switch_interrupt_handler(void);
+void change_temp(void);
+
+
+/* interrupt functions */
+void enable_interrupt_vector(void);
+
+////////////////////////////////////
+
+
+// ### Original Functions Prototypes
+
+// ### Main ###
+void main(void) {
+
+    init_right_switch();
+    enable_interrupt_vector();
+
+    change_temp();
+
+    while(1){
+
+    }
+}
+
+void change_temp(void){
+    temp=882;
+}
+
+
+///////////////////////////////////////
+
+//
+// ### non-override functions
+void stop_watchdog_timer(void){
+    WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
+}
+
+void enable_interrupt_vector(void){
+    __bis_SR_register(GIE);
+}
+
+void init_right_switch(void){
+    P2OUT |= BIT1; // DIR
+    P2REN |= BIT1; // pull up resister
+
+    P2IE |= BIT1; // Interrupt Enable
+    P2IES |= BIT1; // Interrupt edge select : Falling Edge
+    P2IFG &= ~BIT1; // interrupt flag
+}
+
+void right_switch_interrupt_handler(void){
+    temp = 326;
+}
+
+
+// right switch interrupt
+#pragma vector=PORT2_VECTOR
+__interrupt void Port_2(void)
+{
+    if((P2IN & BIT1) == 0)
+    {
+        right_switch_interrupt_handler();
+    }
+    P2IFG &= ~BIT1; // IFG clear (Interrupt END)
+}
+
